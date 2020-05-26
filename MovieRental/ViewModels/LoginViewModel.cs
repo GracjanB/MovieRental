6b12de727d00f1b803f735a39b972e8143430adf
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-05-26 15:38:29 +0200

    Added template for login view

---
+++ MovieRental/ViewModels/LoginViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Caliburn.Micro;
+using MovieRental.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRental.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private readonly ILoggedInUser _userService;
+
+        private readonly IEventAggregator _events;
+
+        public LoginViewModel(ILoggedInUser userService, IEventAggregator eventAggregator)
+        {
+            _userService = userService;
+            _events = eventAggregator;
+        }
+
+        #region Form Controls
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+
+        public void Login()
+        {
+            // TODO: Make login function
+        }
+
+        #endregion
+    }
+}
