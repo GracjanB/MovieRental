36c8d1b2b275b8ad456e6234ab5bc0018a1cf526
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-05-27 17:06:04 +0200

    Implemented template for account view

--- MovieRental/ViewModels/AccountViewModel.cs
+++ MovieRental/ViewModels/AccountViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Caliburn.Micro;
+using MovieRental.Models;
+using MovieRental.User;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +9,53 @@ using System.Threading.Tasks;
 
 namespace MovieRental.ViewModels
 {
-    public class AccountViewModel : Screen
+    public class AccountViewModel : Conductor<object>
     {
+        private readonly SimpleContainer _container;
 
+        private readonly ILoggedInUser _userService;
+
+        public AccountViewModel(SimpleContainer simpleContainer, ILoggedInUser userService)
+        {
+            _container = simpleContainer;
+            _userService = userService;
+        }
+
+
+        #region User Info Card
+
+        public UserModel User { get; set; } = new UserModel();
+
+        public void RechargeAccount()
+        {
+            // TODO
+        }
+
+        #endregion
+
+
+        #region Navigation Menu
+
+        public void UserLibraryShow()
+        {
+            // TODO
+        }
+
+        public void UserReviewsShow()
+        {
+            // TODO
+        }
+
+        public void UserMoviesRentalHistory()
+        {
+            // TODO
+        }
+
+        public void AccountSettingsShow()
+        {
+            // TODO
+        }
+
+        #endregion
     }
 }
