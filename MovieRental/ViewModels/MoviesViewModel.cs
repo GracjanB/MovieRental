2dca3493f1b4ee96e8965fa7f5195a20f8c4f5f8
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-05-29 17:16:31 +0200

    Implemented basic View and logic for MoviesView

--- MovieRental/ViewModels/MoviesViewModel.cs
+++ MovieRental/ViewModels/MoviesViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Caliburn.Micro;
+using MovieRental.Models;
+using MovieRental.User;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,50 @@ namespace MovieRental.ViewModels
 {
     public class MoviesViewModel : Screen
     {
+        private readonly SimpleContainer _container;
 
+        private readonly ILoggedInUser _userService;
+
+        public MoviesViewModel(SimpleContainer container, ILoggedInUser userService)
+        {
+            _container = container;
+            _userService = userService;
+        }
+
+
+        #region Searching Menu
+
+        public string MovieNameSearch { get; set; }
+
+        public BindableCollection<string> CategorySearch { get; set; }
+
+        public string SelectedCategory { get; set; }
+
+        public int ProductionYearFrom { get; set; }
+
+        public int ProductionYearTo { get; set; }
+
+        public void Search()
+        {
+            // TODO
+        }
+
+        #endregion
+
+        #region Movies List
+
+        public BindableCollection<MovieModel> Movies { get; set; }
+
+        public void MovieDetails(MovieModel movie)
+        {
+            // TODO
+        }
+
+        public void MakeRent(MovieModel movie)
+        {
+            // TODO
+        }
+
+        #endregion
     }
 }
