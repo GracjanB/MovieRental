9700a66a5439a00a68c35f86d656dab37ff19b75
Dawidson957 <45687438+Dawidson957@users.noreply.github.com>
2020-05-29 18:08:40 +0200

    Added basic view with logic for movie details

---
+++ MovieRental/ViewModels/MovieDetailsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using MovieRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRental.ViewModels
+{
+    public class MovieDetailsViewModel : Screen
+    {
+        public MovieModel Movie { get; set; }
+
+        public void RentMovie()
+        {
+            // TODO
+        }
+
+        public void MoveBack()
+        {
+            // TODO
+        }
+    }
+}
